08d3e7a86574f69c883d3f027dd6e024d915bc84
Wiebe, Alexander <wiebea3@fcc.ca>
2021-11-18 12:44:45 -0600

    #1235 [feat(ticket components)] whoops
    [ajdub]

--- src/app/ticket/ticket.module.ts
+++ src/app/ticket/ticket.module.ts
@@ -1,9 +1,9 @@
 import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
-import { TicketFormComponent } from "components/ticket-form/ticket-form.component";
-import { TicketListFormComponent } from "components/ticket-list-form/ticket-list-form.component";
-import { TicketListComponent } from "containers/ticket-list/ticket-list.component";
-import { TicketComponent } from "containers/ticket/ticket.component";
+import { TicketFormComponent } from "./components/ticket-form/ticket-form.component";
+import { TicketListFormComponent } from "./components/ticket-list-form/ticket-list-form.component";
+import { TicketListComponent } from "./containers/ticket-list/ticket-list.component";
+import { TicketComponent } from "./containers/ticket/ticket.component";
 
 @NgModule({
   declarations: [
