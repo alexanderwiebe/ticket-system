d7ab8d5216457fee4b4cec056c5b70826a6d901a
Wiebe, Alexander <wiebea3@fcc.ca>
2021-11-18 13:14:27 -0600

    #1235 [feat(ticket components)] forgot the prefix and adapter selectors
    [ajdub]

--- src/app/ticket/store/selectors/ticket.selectors.ts
+++ src/app/ticket/store/selectors/ticket.selectors.ts
@@ -5,6 +5,13 @@ export const selectTicketState = createFeatureSelector<fromTicket.State>(
   fromTicket.ticketFeatureKey
 );
 
+export const {
+  selectIds: selectTicketIds,
+  selectEntities: selectTicketEntities,
+  selectAll: selectAllTickets,
+  selectTotal: selectTotalTickets,
+} = fromTicket.adapter.getSelectors(selectTicketState);
+
 export const selectTicketsLoaded = createSelector(
   selectTicketState,
   (state: fromTicket.State) => state.loaded
