cb45cfc1b33655cdf1650cd5d1023323afee94bf
Wiebe, Alexander <wiebea3@fcc.ca>
2021-11-18 16:11:21 -0600

    #1235 [feat(ticket components)] displaying with ngrx now
    [ajdub]

--- src/app/ticket/components/ticket-list-form/ticket-list-form.component.ts
+++ src/app/ticket/components/ticket-list-form/ticket-list-form.component.ts
@@ -1,16 +1,21 @@
-import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  Input,
+  OnInit,
+} from "@angular/core";
+import { Ticket } from "src/app/backend.service";
 
 @Component({
-  selector: 'app-ticket-list-form',
-  templateUrl: './ticket-list-form.component.html',
-  styleUrls: ['./ticket-list-form.component.css'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  selector: "app-ticket-list-form",
+  templateUrl: "./ticket-list-form.component.html",
+  styleUrls: ["./ticket-list-form.component.css"],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class TicketListFormComponent implements OnInit {
+  @Input() tickets: Ticket[];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
--- src/app/ticket/containers/ticket-list/ticket-list.component.ts
+++ src/app/ticket/containers/ticket-list/ticket-list.component.ts
@@ -1,16 +1,20 @@
-import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from "@angular/core";
+import { select, Store } from "@ngrx/store";
+import { Observable } from "rxjs";
+import { Ticket } from "src/app/backend.service";
+import { selectAllTickets } from "../../store/selectors/ticket.selectors";
 
 @Component({
-  selector: 'app-ticket-list',
-  templateUrl: './ticket-list.component.html',
-  styleUrls: ['./ticket-list.component.css'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  selector: "app-ticket-list",
+  templateUrl: "./ticket-list.component.html",
+  styleUrls: ["./ticket-list.component.css"],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class TicketListComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  public tickets$: Observable<Ticket[]>;
+  constructor(private store: Store) {
+    this.tickets$ = this.store.pipe(select(selectAllTickets));
   }
 
+  ngOnInit(): void {}
 }
